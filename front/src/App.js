281f2ce5354adbde5cfa445ad2868d6f570bbb33
wzz <ywc0008@naver.com>
2024-05-01 21:51:29 +0900

    front 초기 설정 picoCSS 설치

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
