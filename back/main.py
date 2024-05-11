2a421a1bde1c21253b7fdbb6999b19a90e6250cb
wzz <ywc0008@naver.com>
2024-05-12 02:49:19 +0900

    sqlite3 db생성 및 fastapi로 나스닥 100 주가 데이터 api 생성

--- back/main.py
+++ back/main.py
@@ -1,15 +1,30 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import sqlite3
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# SQLite 데이터베이스 연결 함수
+def connect_db():
+    return sqlite3.connect("chart.db")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# API 엔드포인트 작성
+@app.get("/nasdaq_chart")
+async def get_nasdaq_chart():
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("SELECT * FROM stocks ORDER BY date DESC LIMIT 10")
+    rows = c.fetchall()
+    conn.close()
+    if not rows:
+        raise HTTPException(status_code=404, detail="Chart data not found")
+
+    column_names = [description[0] for description in c.description]  # 컬럼명 가져오기
+
+    # 데이터를 JSON 형식으로 변환
+    chart_data = []
+    for row in rows:
+        chart_data.append({column_names[i]: row[i] for i in range(len(row))})
+
+    return chart_data
