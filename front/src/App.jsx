96d40e295d16f71c00465f3f05fe663df253a15c
wzz <ywc0008@naver.com>
2024-06-02 23:40:17 +0900

    주석 지우기

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -30,9 +30,9 @@ function App() {
   
   return (
     <div className="App">
-      {/* <Suspense allback={<h1>데이터 불러오는중</h1>} className="chart">
+      <Suspense allback={<h1>데이터 불러오는중</h1>} className="chart">
         <Chart />
-      </Suspense> */}
+      </Suspense>
         <Suspense fallback={<h1>화면 로딩중</h1>}>
         <UserInput 
           setFirstDate={setFirstDate}
